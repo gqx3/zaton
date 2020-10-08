5be329197bf13e168b6db767bcf4300062dcd7d8
gqx3 <72559213+gqx3@users.noreply.github.com>
2020-10-08 22:45:59 +0400

    add uniform mat4 to shaders

--- data/shaders/basic_fragment.glsl
+++ data/shaders/basic_fragment.glsl
@@ -4,5 +4,5 @@ out vec4 frag_col;
 
 void main()
 {
-    frag_col = vec4(vec3(1.0), 1.0);
+    frag_col = vec4(vec3(.7), 1.0);
 }
--- data/shaders/basic_vertex.glsl
+++ data/shaders/basic_vertex.glsl
@@ -2,7 +2,9 @@
 
 layout(location=0) in vec3 in_position;
 
+uniform mat4 mvp;
+
 void main()
 {
-    gl_Position = vec4(in_position, 1.0);
+    gl_Position = mvp * vec4(in_position, 1.0);
 }
